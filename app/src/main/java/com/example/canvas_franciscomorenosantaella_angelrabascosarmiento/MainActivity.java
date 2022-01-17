8e07864db8fca3fb97a046f08998acc8ca20637f
FranciscoMorenoSantaella <santaellamorenofrancisco@gmail.com>
2022-01-17 10:27:17 +0100

    cocacola espuma

--- app/src/main/java/com/example/canvas_franciscomorenosantaella_angelrabascosarmiento/MainActivity.java
+++ app/src/main/java/com/example/canvas_franciscomorenosantaella_angelrabascosarmiento/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
@@ -13,12 +14,23 @@ import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    MediaPlayer mp;
+    MediaPlayer mp2;
+    MediaPlayer mp3;
+    MediaPlayer mp4;
+    MediaPlayer mp5;
     private LinearLayout canvasLayout;
     public Button button;
     private DisplayMetrics displaymetrics;
     public int cantidad = 0;
-    public int  altura;
+    public int ancho;
+    public int altura;
+    public Button rellenar;
+    public Button cola;
+    public Button vino;
+    public int bebida = Color.CYAN;
+    public Button water;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,9 +38,16 @@ public class MainActivity extends AppCompatActivity {
         this.canvasLayout = (LinearLayout) findViewById(R.id.canvasLayout);
 
         Vista vista = new Vista(this);
-
+        mp = MediaPlayer.create(this, R.raw.drinksound);
         this.canvasLayout.addView(vista);
         button = findViewById(R.id.Beber);
+        rellenar = findViewById(R.id.Rellenar);
+        mp2 = MediaPlayer.create(this, R.raw.rellenarsound);
+        cola = findViewById(R.id.Cocacola);
+        vino = findViewById(R.id.Vino);
+        mp3 = MediaPlayer.create(this, R.raw.cocacolaespuma);
+        mp4 = MediaPlayer.create(this, R.raw.winesound);
+        mp5 = MediaPlayer.create(this, R.raw.drinkwatersound);
 
     }
 
@@ -38,10 +57,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         public void onDraw(Canvas canvas) {
-            int ancho = canvas.getWidth();
+            ancho = canvas.getWidth();
             altura = canvas.getHeight();
             Paint paint = new Paint();
-            paint.setColor(Color.YELLOW);
+            paint.setColor(bebida);
             paint.setStyle(Paint.Style.FILL);
             canvas.drawRect(ancho, altura / 2, ancho / 2, cantidad, paint);
 
@@ -50,34 +69,69 @@ public class MainActivity extends AppCompatActivity {
             paint.setColor(Color.BLACK);
 
 
-
             canvas.drawRect(ancho, altura / 2, ancho / 2, 0, paint);
 
 
-
         }
-
-        //Button button = findViewById(R.id.Beber);
 
 
     }
 
     public void gluglu(View view) {
 
-            if (cantidad < altura/2) {
-                if(cantidad +10 > altura/2){
-                    cantidad = altura/2;
-                }
-                cantidad += 25;
-            } else {
-                Toast.makeText(this, "Te lo has bebido todo", Toast.LENGTH_SHORT).show();
+        mp.start();
+        if (cantidad < altura / 2) {
+            if (cantidad + 10 > altura / 2) {
+                cantidad = altura / 2;
             }
-
-            Vista vista = new Vista(this);
-            this.canvasLayout.removeAllViews();
-            this.canvasLayout.addView(vista);
-
-
+            cantidad += 25;
+        } else {
+            Toast.makeText(this, "Te lo has bebido todo", Toast.LENGTH_SHORT).show();
         }
 
+        Vista vista = new Vista(this);
+        this.canvasLayout.removeAllViews();
+        this.canvasLayout.addView(vista);
+
+
     }
+
+    public void echarLiquido(View view){
+       mp2.start();
+        cantidad = 0;
+
+
+
+        Vista vista = new Vista(this);
+        this.canvasLayout.removeAllViews();
+        this.canvasLayout.addView(vista);
+
+    }
+
+    public void colorCola(View view){
+        mp3.setVolume(0.2f,0.2f);
+        mp3.start();
+        bebida = Color.rgb	(111,23,18);
+        Vista vista = new Vista(this);
+        this.canvasLayout.removeAllViews();
+        this.canvasLayout.addView(vista);
+    }
+
+    public void colorVino(View view){
+        mp4.start();
+        bebida = Color.rgb(80, 19, 86);
+        Vista vista = new Vista(this);
+        this.canvasLayout.removeAllViews();
+        this.canvasLayout.addView(vista);
+    }
+
+    public void colorWater(View view){
+        mp5.start();
+        bebida = Color.CYAN;
+        Vista vista = new Vista(this);
+        this.canvasLayout.removeAllViews();
+        this.canvasLayout.addView(vista);
+    }
+
+
+}
